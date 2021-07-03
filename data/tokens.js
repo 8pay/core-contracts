5c133f68d4290f4051ec9af98f4b57f91fee5df6
AleG94 <alegen.dev@gmail.com>
2021-07-03 12:48:14 +0200

    chore(tokens): update private network tokens

--- data/tokens.js
+++ data/tokens.js
@@ -1,9 +1,7 @@
 module.exports = {
   hardhat: [],
   private: [
-    '0x6073fd5461e09fc874a0880daf7e174d25228892',
-    '0xbcfee3c6de134d2db1fa7341615cab8ab082196e',
-    '0xae13d989dac2f0debff460ac112a837c89baa7cd',
-    '0x0d338e1a67966c995d754e06bd53fa5e59c1988b'
+    '0xaF7e5080C403b26f66F55eba3e15a6669cc20e3e',
+    '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd'
   ]
 };
